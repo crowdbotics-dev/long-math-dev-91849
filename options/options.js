dfeb8ce20d47a09d1e1a4cab3769b4a956b6a74b
Crowdbotics <engineering@crowdbotics.com>
2023-06-15 23:07:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-math-dev-91849",
+  url: "https://long-math-dev-91849.botics.co",
+  api: "https://long-math-dev-91849.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
